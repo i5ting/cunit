77df7c6f394955a4c83bd2d38709273bd49483d9
i5ting <i5ting@126.com>
2017-01-26 18:07:06 +0800

    lazycommmit, please ignore

--- cli.js
+++ cli.js
@@ -7,13 +7,22 @@ argv.shift()
 var filePath = __dirname
 var currentPath = process.cwd()
 
+let flag = false
+
 for(var i in argv){
   var _argv = argv[i];
   if(_argv == '-m'  || _argv == '--min'){
+    flag = true
     fs.createReadStream(filePath + '/tpl/minunit.h').pipe(fs.createWriteStream(currentPath + '/minunit.h'))
   }
   
   if(_argv == '-g'  || _argv == '--greate'){
+    flag = true
     fs.createReadStream(filePath + '/tpl/greatest.h').pipe(fs.createWriteStream(currentPath + '/greatest.h'))
   }
+}
+
+if (flag === false) {
+  console.log('cunit -m: create minunit.h')
+  console.log('cunit -g: create greatest.h')
 }
